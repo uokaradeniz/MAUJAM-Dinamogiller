9fd0296cd03273eb7bff845a05913985aa3455de
uokaradeniz <uokaradeniz@gmail.com>
2023-06-05 20:00:26 +0300

    asdf

--- Assets/Oguzhan/new UI/mainMenuScript.cs
+++ Assets/Oguzhan/new UI/mainMenuScript.cs
@@ -1,21 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 public class mainMenuScript : MonoBehaviour
 {
-
-
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log("game started");
     }
 
     public void exitGame()
     {
-        Debug.Log("quit");
         Application.Quit();
+    }
+
+    public void SoundOn()
+    {
+        AudioListener.volume = 1;
+    }
+
+    public void SoundOff()
+    {
+        AudioListener.volume = 0;
     }
 }
 
